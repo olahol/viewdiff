e432055050b5bea99c94d5ee71a43d1d9b49b2ef
Eric Hagman <emhagman@gmail.com>
2015-10-21 13:18:35 -0400

    CLI - Print warning when diff is found
    Instead of opening the program when there is no diff, it simply
    writes to the console that there are no changes. Saves time.

--- cli.js
+++ cli.js
@@ -7,10 +7,25 @@ var fs = require("fs")
 var serverPath = path.join(__dirname, "./server.js")
 var args = [serverPath];
 
-if (!process.stdin.isTTY) {
-  var proc = spawn(electron, args)
-  process.stdin.pipe(proc.stdin)
-} else {
-  console.error("No stdin provided");
-  process.exit(1);
-}
+var hasRead = false;
+process.stdin.on('readable', function () {
+  var partial = process.stdin.read(1);
+  if (!partial) {
+    console.error("Your branch has no changes to view.");
+    process.exit(0);
+  }
+  if (partial && !hasRead) {
+    partial = partial.toString().trim();
+    hasRead = true;
+    if (!process.stdin.isTTY && partial !== "") {
+      var proc = spawn(electron, args)
+      process.stdin.pipe(proc.stdin)
+    } else if (partial === "") {
+      console.error("Your branch has no changes to view.");
+      process.exit(0);
+    } else {
+      console.error("No stdin provided");
+      process.exit(1);
+    }
+  }
+});
